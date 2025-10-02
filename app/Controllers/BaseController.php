39464b1f6de712cf8c47e1022d38102acf8001c3
Petr Vaněk <krteczek01@gmail.com>
2025-10-02 09:42:22 +0200

    Update BaseController.php
    přidány komentáře a poznámky

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -8,40 +8,77 @@ use App\Core\Template;
 use App\Core\Config;
 use App\Auth\LoginService;
 
+/**
+ * Abstraktní základní controller pro společné funkce všech controllerů
+ *
+ * Poskytuje základní funkcionalitu pro renderování stránek,
+ * správu šablon a přístup k autentizační službě.
+ * Všechny specifické controllery by měly dědit z této třídy.
+ *
+ * @package App\Controllers
+ * @author KRS3
+ * @version 3.0
+ */
 abstract class BaseController
 {
+    /**
+     * @param Template $template Šablonovací systém
+     * @param string $baseUrl Základní URL aplikace
+     * @param LoginService $authService Služba pro autentizaci uživatelů
+     */
     public function __construct(
         protected Template $template,
         protected string $baseUrl,
         protected LoginService $authService // ← PŘIDEJ
     ) {}
 
-protected function renderPage(string $contentView, array $contentData = [], string $pageKey = 'home', array $titleParams = []): string
-{
-    $siteName = Config::site('name');
+    /**
+     * Vykreslí kompletní HTML stránku s layoutem
+     *
+     * Zkombinuje obsahovou část s layoutem, přidá metadata,
+     * title a uživatelská data. Používá se pro frontend stránky.
+     *
+     * @param string $contentView Cesta k šabloně obsahu (např. 'home.php')
+     * @param array $contentData Data specifická pro obsahovou část
+     * @param string $pageKey Klíč pro lokalizaci názvu stránky (např. 'home', 'login')
+     * @param array $titleParams Parametry pro dynamický title stránky
+     * @return string Kompletní vyrenderované HTML
+     *
+     * @example
+     * $this->renderPage('home.php', ['articles' => $articles], 'home');
+     * $this->renderPage('article_detail.php', $articleData, 'article_detail', ['title' => $articleTitle]);
+     */
+    protected function renderPage(string $contentView, array $contentData = [], string $pageKey = 'home', array $titleParams = []): string
+    {
+        // Získá název webu z konfigurace
+        $siteName = Config::site('name');
 
-    $title = Config::text('pages.' . $pageKey, array_merge([
-        'site_name' => $siteName
-    ], $titleParams), $siteName);
+        // Sestaví title stránky s podporou lokalizace
+        $title = Config::text('pages.' . $pageKey, array_merge([
+            'site_name' => $siteName
+        ], $titleParams), $siteName);
 
-    $userData = $this->authService->getUser();
+        // Získá data o přihlášeném uživateli
+        $userData = $this->authService->getUser();
 
-    $layoutData = [
-        'title' => $title,
-        'content' => $this->template->render($contentView, array_merge($contentData, [
+        // Připraví data pro layout
+        $layoutData = [
+            'title' => $title,
+            'content' => $this->template->render($contentView, array_merge($contentData, [
+                'baseUrl' => $this->baseUrl,
+                'siteName' => $siteName
+            ])),
             'baseUrl' => $this->baseUrl,
-            'siteName' => $siteName
-        ])),
-        'baseUrl' => $this->baseUrl,
-        'siteName' => $siteName,
-        'user' => [
-            'isLoggedIn' => $this->authService->isLoggedIn(),
-            'username' => $userData['username'] ?? null, // ← z getUser()
-            'id' => $userData['id'] ?? null,
-            'role' => $userData['role'] ?? null
-        ]
-    ];
+            'siteName' => $siteName,
+            'user' => [
+                'isLoggedIn' => $this->authService->isLoggedIn(),
+                'username' => $userData['username'] ?? null, // ← z getUser()
+                'id' => $userData['id'] ?? null,
+                'role' => $userData['role'] ?? null
+            ]
+        ];
 
-    return $this->template->render('layouts/frontend.php', $layoutData);
-}
+        // Vykreslí kompletní stránku s layoutem
+        return $this->template->render('layouts/frontend.php', $layoutData);
+    }
 }
